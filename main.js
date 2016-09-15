82ecd5156bcb589c1f980dd354123e21f4aecdfe
Gavin Wu <gavin@ciwic.com>
2016-09-15 11:58:08 +1000

    put the js to a IIFE to avoid confilcting

--- main.js
+++ main.js
@@ -10,103 +10,105 @@ Resources at your disposal:
  - Google - feel free to use it, any dev who claims not to use stack overflow is a liar or a goose
  - mockdata and a scaffold dataloaded handler
 */
-'use strict';
 
-// Handy polyfill for formatting strings
-// e.g. "Hiya {0} {1}".format("odd", "chap"); // "Hiya odd chap"
-var formatString = function (str) {
-  var reg;
-  var values = Array.prototype.slice.call(arguments, 1);
-  var i;
-  for (i = 0; i < values.length; i++) {
-    reg = new RegExp("\\{" + i + "\\}", "gm");
-    str = str.replace(reg, values[i]);
-  }
-  return str;
-}
+(function () {
+  'use strict';
 
-var createChart = function (data, target) {
-  $(target).highcharts(data);
-}
+  // Handy polyfill for formatting strings
+  // e.g. "Hiya {0} {1}".format("odd", "chap"); // "Hiya odd chap"
+  var formatString = function (str) {
+    var reg;
+    var values = Array.prototype.slice.call(arguments, 1);
+    var i;
+    for (i = 0; i < values.length; i++) {
+      reg = new RegExp("\\{" + i + "\\}", "gm");
+      str = str.replace(reg, values[i]);
+    }
+    return str;
+  }
 
-var buildTitles = function (data, responseObj) {
-  data.title = {
-    text: formatString('Revenue and Expenses for {0}', responseObj["OrgName"]),
-    x: -20 //center
+  var createChart = function (data, target) {
+    $(target).highcharts(data);
   }
-  data.subtitle = {
-    text: '',
-    x: -20
-  }
-}
 
-var buildAxises = function (data, responseObj) {
-  data.xAxis = {
-    categories: responseObj.Cashflow.map(function getPeriodName(monthDataItem) {
-      return monthDataItem.Label;
-    })
-  }
-  data.yAxis = {
-    title: {
-      text: formatString('Currency ({0})', responseObj["Currency"])
+  var buildTitles = function (data, responseObj) {
+    data.title = {
+      text: formatString('Revenue and Expenses for {0}', responseObj["OrgName"]),
+      x: -20 //center
+    }
+    data.subtitle = {
+      text: '',
+      x: -20
     }
   }
-}
 
-var buildLegneds = function (data, responseObj) {
-  data.tooltip = {
-    valueSuffix: ''
-  }
-  data.legend = {
-    layout: 'vertical',
-    align: 'right',
-    verticalAlign: 'middle',
-    borderWidth: 0
-  }
-}
-
-var buildSerieses = function (data, responseObj) {
-  data.series = [{
-    name: 'Revenue',
-    color: '#00AA00',
-    data: responseObj.Cashflow.map(function getPeriodRevenueFigures(monthDataItem) {
-      return monthDataItem.Series[0].Value;
-    })
-  },
-    {
-      name: 'Expenses',
-      color: '#AA0000',
-      data: responseObj.Cashflow.map(function getPeriodExpenseFigures(monthDataItem) {
-        if (monthDataItem.Series[1] === undefined) {
-          return 0;
-        }
-        return monthDataItem.Series[1].Value;
+  var buildAxises = function (data, responseObj) {
+    data.xAxis = {
+      categories: responseObj.Cashflow.map(function getPeriodName(monthDataItem) {
+        return monthDataItem.Label;
       })
-    }];
-}
+    }
+    data.yAxis = {
+      title: {
+        text: formatString('Currency ({0})', responseObj["Currency"])
+      }
+    }
+  }
 
-var buildCharData = function(response){
-  var responseObj = JSON.parse(response);
-  var data = {};
+  var buildLegneds = function (data, responseObj) {
+    data.tooltip = {
+      valueSuffix: ''
+    }
+    data.legend = {
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'middle',
+      borderWidth: 0
+    }
+  }
 
-  buildTitles(data, responseObj);
+  var buildSerieses = function (data, responseObj) {
+    data.series = [{
+      name: 'Revenue',
+      color: '#00AA00',
+      data: responseObj.Cashflow.map(function getPeriodRevenueFigures(monthDataItem) {
+        return monthDataItem.Series[0].Value;
+      })
+    },
+      {
+        name: 'Expenses',
+        color: '#AA0000',
+        data: responseObj.Cashflow.map(function getPeriodExpenseFigures(monthDataItem) {
+          if (monthDataItem.Series[1] === undefined) {
+            return 0;
+          }
+          return monthDataItem.Series[1].Value;
+        })
+      }];
+  }
 
-  buildAxises(data, responseObj);
+  var buildCharData = function (response) {
+    var responseObj = JSON.parse(response);
+    var data = {};
 
-  buildLegneds(data, responseObj);
+    buildTitles(data, responseObj);
 
-  buildSerieses(data, responseObj);
-  return data;
-}
+    buildAxises(data, responseObj);
 
-function onDataLoaded(response) {
-  var data = buildCharData(response);
-  createChart(data, '#container');
-}
+    buildLegneds(data, responseObj);
+
+    buildSerieses(data, responseObj);
+    return data;
+  }
+
+  function onDataLoaded(response) {
+    var data = buildCharData(response);
+    createChart(data, '#container');
+  }
 
 
-$(function () {
-  var data = getMockData();
-  onDataLoaded(data);
-});
-
+  $(function () {
+    var data = getMockData();
+    onDataLoaded(data);
+  });
+})();
