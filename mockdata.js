69c99894cf41e21020dbfb1f52437d3471f39769
Gavin Wu <gavin@ciwic.com>
2016-09-15 12:06:17 +1000

    change the double quotes to single quotes

--- mockdata.js
+++ mockdata.js
@@ -4,88 +4,88 @@
 /* Mocked Ajax response with data for rendering the chart. This is what the API returns so you have to work with this data structure */
 function getMockData() {
   return JSON.stringify({
-    'OrgName': 'Walt\'s Meth Lab',
-    'Currency': 'USD',
-    'Cashflow': [
+    "OrgName": "Walt\'s Meth Lab",
+    "Currency": "USD",
+    "Cashflow": [
       {
-        'Label': 'October',
-        'Series': [
+        "Label": "October",
+        "Series": [
           {
-            'Type': 'Revenue',
-            'Value': 2538.458
+            "Type": "Revenue",
+            "Value": 2538.458
           },
           {
-            'Type': 'Expenses',
-            'Value': 444.78
+            "Type": "Expenses",
+            "Value": 444.78
           }
         ]
       },
       {
-        'Label': 'November',
-        'Series': [
+        "Label": "November",
+        "Series": [
           {
-            'Type': 'Revenue',
-            'Value': 859
+            "Type": "Revenue",
+            "Value": 859
           },
           {
-            'Type': 'Expenses',
-            'Value': 8585
+            "Type": "Expenses",
+            "Value": 8585
           },
           {
-            'Type': 'Projected',
-            'Value': 8585
+            "Type": "Projected",
+            "Value": 8585
           }
         ]
       },
       {
-        'Label': 'December',
-        'Series': [
+        "Label": "December",
+        "Series": [
           {
-            'Type': 'Revenue',
-            'Value': 4458.3
+            "Type": "Revenue",
+            "Value": 4458.3
           }
         ]
       },
       {
-        'Label': 'January',
-        'Series': [
+        "Label": "January",
+        "Series": [
           {
-            'Type': 'Revenue',
-            'Value': 4458
+            "Type": "Revenue",
+            "Value": 4458
           },
           {
-            'Type': 'Expenses',
-            'Value': 7741
+            "Type": "Expenses",
+            "Value": 7741
           }
         ]
       },
       {
-        'Label': 'February',
-        'Series': [
+        "Label": "February",
+        "Series": [
           {
-            'Type': 'Revenue',
-            'Value': 8459.58
+            "Type": "Revenue",
+            "Value": 8459.58
           },
           {
-            'Type': 'Expenses',
-            'Value': 1221
+            "Type": "Expenses",
+            "Value": 1221
           }
         ]
       },
       {
-        'Label': 'March',
-        'Series': [
+        "Label": "March",
+        "Series": [
           {
-            'Type': 'Revenue',
-            'Value': 11489.5
+            "Type": "Revenue",
+            "Value": 11489.5
           },
           {
-            'Type': 'Expenses',
-            'Value': 587
+            "Type": "Expenses",
+            "Value": 587
           },
           {
-            'Type': 'Projected',
-            'Value': 2500
+            "Type": "Projected",
+            "Value": 2500
           }
         ]
       }
